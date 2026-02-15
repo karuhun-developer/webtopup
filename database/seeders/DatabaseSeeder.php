21556a41dfa27686eb80905c0339da920b3e6d5b
bayurifkialghifari <bayurifkialgh@gmail.com>
2026-02-15 17:39:21 +0700

    add admin menu seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             UserSeeder::class,
             SuperadminMenuSeeder::class,
+            AdminMenuSeeder::class,
             SettingSeeder::class,
         ]);
     }
